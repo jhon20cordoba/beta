06642a88b2aee65acd0c9d1f7e8986afab803b33
jhon <baironyon662@gmail.com>
2024-09-20 12:18:46 -0500

    Endpoint : Database connection

---
+++ api/config/db_connection.php
@@ -0,0 +1,25 @@
+<?php
+/*database connection
+developer: jhon*/
+$host = "localhost";
+$username = "postgres";
+$password = "unicesmag";
+$dbname = "beta";
+$port = "5432";
+
+$data_connection = "
+host=$host
+ port=$port
+ dbname=$dbname 
+ user=$username
+ password=$password
+ ";
+ $conn = pg_connect($data_connection);
+ if (!$conn) {
+     die("Connection failed: ". pg_last_error());
+ }else{
+    echo "Connected successfully";
+ }
+
+ pg_close($conn);
+?>
